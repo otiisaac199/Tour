448eea485219956974bd8ed46042e548b55c3f6a
otiisaac199 <otichinazaisaac19@gmail.com>
2022-09-28 15:31:18 +0100

    fetched from api

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
+const Tour = ({ id, image, infp, price }) => {
   return <h2>tour component</h2>;
 };
 
