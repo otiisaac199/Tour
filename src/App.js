1564c10cc89acda11f610454b19ab370cd4a8de3
otiisaac199 <otichinazaisaac19@gmail.com>
2022-09-28 16:20:40 +0100

    features: buton functions done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
   const fetchTours = async () => {
     setLoading(true);
 
@@ -36,7 +41,7 @@ function App() {
   }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Tour = ({ id, image, info, price, name }) => {
+const Tour = ({ id, image, info, price, name, removeTour }) => {
   const [readMore, setReadMore] = useState(false);
 
   return (
@@ -11,8 +11,15 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{readMore ? info : `${info.substring(0, 180)}... `}</p>
-        <button className="delete-btn">not interested</button>
+        <p>
+          {readMore ? info : `${info.substring(0, 180)}... `}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          not interested
+        </button>
       </footer>
     </article>
   );
